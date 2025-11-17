89cf55b0b8b3fedd48f49f0139c2ee54debf8edd
Pablo Gustavo Fernandes Maia <pablo1gustavo.dev@gmail.com>
2025-11-17 15:50:00 -0300

    Adiciona estrutura base do projeto

---
+++ src/main/java/chess/openjml/App.java
@@ -0,0 +1,13 @@
+package chess.openjml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
