17b7be15c3ac79e2ed453e06b8937e5061ca1ac6
Pablo Gustavo Fernandes Maia <pablo1gustavo.dev@gmail.com>
2025-12-07 19:32:45 -0300

    feat: add specifications to Color enum

--- src/main/java/chess/openjml/pieces/enums/Color.java
+++ src/main/java/chess/openjml/pieces/enums/Color.java
@@ -1,7 +1,15 @@
 package chess.openjml.pieces.enums;
 
-public  enum Color
+public enum Color
 {
     BLACK,
     WHITE;
+    
+    //@ ensures this == WHITE ==> \result == 1;
+    //@ ensures this == BLACK ==> \result == -1;
+    //@ pure
+    public int direction()
+    {
+        return this == WHITE ? 1 : -1;
+    }
 }
